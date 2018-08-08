7703aa58fa340d54a8482631134359e2769a96d4
dayjiang <dayjiang@cisco.com>
2018-08-08 11:29:08 -0700

    fix sonar issues

--- src/main/java/com/cisco/gungnir/utils/TimeConverter.java
+++ src/main/java/com/cisco/gungnir/utils/TimeConverter.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.TimeZone;
 
 public class TimeConverter implements Serializable {
-    public static List<String> fromFormatStrings = Arrays.asList("yyyy-MM-dd'T'HH:mm:ss'Z'", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    private static final List<String> fromFormatStrings = Arrays.asList("yyyy-MM-dd'T'HH:mm:ss'Z'", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
     private SimpleDateFormat toFormat;
 
     public TimeConverter() {};
