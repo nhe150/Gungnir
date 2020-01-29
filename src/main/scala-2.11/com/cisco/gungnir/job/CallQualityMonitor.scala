1cb434cd53c93c1e856e9701f85b82782df1f762
srinekk <srinekk@cisco.com>
2020-01-28 21:13:00 -0800

    added the new *monitor.scala files into the project

---
+++ src/main/scala-2.11/com/cisco/gungnir/job/CallQualityMonitor.scala
@@ -0,0 +1,65 @@
+package com.cisco.gungnir.job
+
+import org.apache.spark.sql.{Dataset, Row}
+import org.apache.spark.sql.types.DataTypes
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import java.util
+import java.util.logging.Logger
+
+import com.cisco.gungnir.utils.DateUtil.isBusinessDay
+import org.apache.spark.sql.functions._
+
+object CallQualityMonitor {
+  val LOGGER = Logger.getLogger(classOf[CallQualityMonitor].getName)
+}
+
+class CallQualityMonitor() extends DataMonitor {
+
+  private def getSumPerOrg(ds: Dataset[_], currentDate: String) = {
+    val sumPerOrg = ds.groupBy("orgid", "pdate").agg(
+      sum("duration").as("duration"),
+      sum("audio_is_good").as("audio_is_good"))
+    // note: the # of DISTINCT deviceID could change everyday.)
+
+    System.out.println("@@@@@@@@ getSumPerOrg: @@@@@@@@@@@@")
+    sumPerOrg.show()
+    sumPerOrg
+  }
+
+  @throws[Exception]
+  override def run(xcurrentDate: String, threshold: String, orgId: String): Unit = {
+
+    val myDate = if (xcurrentDate == null)
+      new DateTime(DateTimeZone.UTC).plusDays(-1).toString("yyyy-MM-dd")
+    else
+      xcurrentDate
+
+
+    CallQualityMonitor.LOGGER.info("Entering program. currentDate: " + myDate)
+    if (!isBusinessDay(myDate)) return
+
+    //need to scrutinize
+    val input = readFromCass().toDF()
+    val orgList = getOrgList(configProvider.getAppConfig)
+    val whereOrgIdClause = whereOrgId(orgList)
+
+    val dsFilteredByOrgs = input.where("orgid in (" + whereOrgIdClause + ") and relation_name='callQuality' ")
+
+    val sumPerOrg = getSumPerOrg(dsFilteredByOrgs, myDate)
+    val activeUserMsges = createMessages(sumPerOrg, true)
+    writeToKafka(activeUserMsges, false)
+  }
+
+  @throws[Exception]
+  private def createMessages(dataset: Dataset[Row], alert: Boolean) = {
+    val message = dataset.selectExpr("'crs' as component",
+      "'metrics' as eventtype",
+      "struct('activeUser' as pipeLine, " +
+        "struct(" + "orgid, " + "duration, " + "audio_is_good, " +
+        "pdate as reportDate )" +
+        " as data ) " +
+        "as metrics")
+    message
+  }
+}
---
+++ src/main/scala-2.11/com/cisco/gungnir/job/VideoUsageMonitor.scala
@@ -0,0 +1,64 @@
+package com.cisco.gungnir.job
+
+import org.apache.spark.sql.{Dataset, Row}
+import org.apache.spark.sql.types.DataTypes
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import java.util
+import java.util.logging.Logger
+
+import com.cisco.gungnir.utils.DateUtil.isBusinessDay
+import org.apache.spark.sql.functions._
+
+object VideoUsageMonitor {
+  val LOGGER = Logger.getLogger(classOf[VideoUsageMonitor].getName)
+}
+
+class VideoUsageMonitor() extends DataMonitor {
+
+  private def getSumPerOrg(ds: Dataset[_], currentDate: String) = {
+    val sumPerOrg = ds.groupBy("orgid", "pdate").agg(
+      sum("video time").as("video time "))
+    // note: the # of DISTINCT deviceID could change everyday.)
+
+    System.out.println("@@@@@@@@ getSumPerOrg: @@@@@@@@@@@@")
+    sumPerOrg.show()
+    sumPerOrg
+  }
+
+  @throws[Exception]
+  override def run(xcurrentDate: String, threshold: String, orgId: String): Unit = {
+
+    val myDate = if (xcurrentDate == null)
+      new DateTime(DateTimeZone.UTC).plusDays(-1).toString("yyyy-MM-dd")
+    else
+      xcurrentDate
+
+
+    VideoUsageMonitor.LOGGER.info("Entering program. currentDate: " + myDate)
+    if (!isBusinessDay(myDate)) return
+
+    //need to scrutinize
+    val input = readFromCass().toDF()
+    val orgList = getOrgList(configProvider.getAppConfig)
+    val whereOrgIdClause = whereOrgId(orgList)
+
+    val dsFilteredByOrgs = input.where("orgid in (" + whereOrgIdClause + ") and relation_name='videoUsage' ")
+
+    val sumPerOrg = getSumPerOrg(dsFilteredByOrgs, myDate)
+    val activeUserMsges = createMessages(sumPerOrg, true)
+    writeToKafka(activeUserMsges, false)
+  }
+
+  @throws[Exception]
+  private def createMessages(dataset: Dataset[Row], alert: Boolean) = {
+    val message = dataset.selectExpr("'crs' as component",
+      "'metrics' as eventtype",
+      "struct('activeUser' as pipeLine, " +
+        "struct(" + "orgid, " + "videotime, " +
+        "pdate as reportDate )" +
+        " as data ) " +
+        "as metrics")
+    message
+  }
+}
